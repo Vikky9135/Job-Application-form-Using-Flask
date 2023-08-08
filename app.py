36eb0e59d44d85dcad0ffe03017aacb070cff253
Vikram <vikramvikky2003@gmail.com>
2023-08-08 22:51:21 +0530

    Storing the user data in the database.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -25,7 +26,13 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
 
     return render_template("index.html")
 
