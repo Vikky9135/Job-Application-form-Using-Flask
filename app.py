5921bfcf72c70e63834ba47f52737785789633b0
Vikram <vikramvikky2003@gmail.com>
2023-08-08 18:21:03 +0530

    Getting the user data.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST" :
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
+
 
 app.run(debug=True, port=5001)
