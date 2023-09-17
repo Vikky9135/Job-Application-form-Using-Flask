c6a75be977e732d9e4ab0c8e73fec043ac0d9ef7
Vikram <vikramvikky2003@gmail.com>
2023-09-17 22:38:00 +0530

    Final commit.

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, flash
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "Vikram123"
+app.config["SECRET_KEY"] = "myapplication123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "pythonemailmanager@gmail.com"
+app.config["MAIL_PASSWORD"] = "nfqsdpvypfuokxzv"
 
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -21,7 +29,7 @@ class Form(db.Model):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST" :
+    if request.method == "POST":
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
         email = request.form["email"]
@@ -33,9 +41,26 @@ def index():
                     email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"Thank you for your submission, {first_name}. " \
+                       f"Here are your data:\n{first_name}\n{last_name}\n{date}\n" \
+                       f"Thank you!"
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
         flash(f"{first_name}, your form was submitted successfully!", "success")
 
+        return redirect(url_for("success"))
+
     return render_template("index.html")
+
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
 
 
 if __name__ == "__main__":
